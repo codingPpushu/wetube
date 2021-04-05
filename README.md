878e74f633593971e7241443d1e58aa91d3384a8
codingPpushu <codingPpushu@gmail.com>
2021-04-06 00:55:54 +0900

    nodejs, expressjs, mongodb, webpack implement

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # Wetube
 
 Cloning Youtube with Vanilla and NodeJs
+
+## Pages:
+
+- [ ] Home
+- [x] Join
+- [x] Login
+- [x] Search
+- [ ] User Detail
+- [x] Edit Profile
+- [x] Change Password
+- [x] Upload
+- [ ] Veideo Detail
+- [x] Edit Video
---
+++ app.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
+import { localsMiddleware } from './middlewares';
+import routes from './routes';
+import userRouter from './routers/userRouter';
+import videoRouter from './routers/videoRouter';
+import globalRouter from './routers/globalRouter';
+const app = express();
+
+app.use(function (req, res, next) {
+  res.setHeader(
+    'Content-Security-Policy',
+    "script-src 'self' https://archive.org",
+  );
+  return next();
+});
+app.set('view engine', 'pug');
+app.use('/uploads', express.static('uploads'));
+app.use('/static', express.static('static'));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+
+app.use(localsMiddleware);
+
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
+
+export default app;
