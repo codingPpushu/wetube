75aec361de4fcd2095a7e3ec225cbbb7ef40a5a1
codingPpushu <codingPpushu@gmail.com>
2021-04-08 01:55:02 +0900

    User-Authentication__passport,login, user, express

--- app.js
+++ app.js
@@ -3,17 +3,22 @@ import morgan from 'morgan';
 import helmet from 'helmet';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
+import passport from 'passport';
+import session from 'express-session';
 import { localsMiddleware } from './middlewares';
 import routes from './routes';
 import userRouter from './routers/userRouter';
 import videoRouter from './routers/videoRouter';
 import globalRouter from './routers/globalRouter';
+
+import './passport';
+
 const app = express();
 
 app.use(function (req, res, next) {
   res.setHeader(
     'Content-Security-Policy',
-    "script-src 'self' https://archive.org",
+    "script-src 'self' https://archive.org"
   );
   return next();
 });
@@ -24,6 +29,15 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('dev'));
+app.use(
+  session({
+    secret: process.env.COOKIE_SECRET,
+    resave: true,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(localsMiddleware);
 
